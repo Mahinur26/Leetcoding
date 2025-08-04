8053adbec3215e9639978ee4c5da3919d0c36e93
Mahinur26 <mahinurmahi26@gmail.com>
2025-08-04 01:18:31 -0400

    Optimal solution for group anagrams. Did my solution for Top K Freq Elements

--- Group Anagrams/Group Anagrams.py
+++ Group Anagrams/Group Anagrams.py
@@ -1,11 +1,11 @@
 #Medium
 
-#My solution/less efficient solution:
+#My solution/Less optimal solution:
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         mapOfAnagrams = {} #sortedWord tuple: Word str
         for word in strs:
-            #Lists aren't hashable, but tuples are as they are immutable
+            #Lists aren't hashable, but tuples are as they are immutable - keys must be immutable
             #The sorted() function sorts the chars in a list like ['a', 'c', 'e']
             key = tuple(sorted(word))
             if key in mapOfAnagrams:
@@ -18,3 +18,28 @@ class Solution:
 
 #Time Complexity: O(n log n) 
 #Space Complexity: O(n) 
+
+#Optimal solution:
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagramsDict = defaultdict(list)
+        for word in strs:
+            #Creates an array for word and its chars with 26 0's to represent each letter in the alphabet
+            count = [0] * 26
+            for char in word:
+                #Subtracting the ASCII values (with ord() function) from lowercase a-z
+                #These values are contiguous so the subtraction will output an index from 0-25 inclusive
+                #Adding 1 to the index for the corressponding character
+                count[ord(char) - ord('a')] += 1.
+            
+            key = tuple(count)
+            #If the count is already in the dictionary, then the word will be appended
+            #Otherwise, a new key-value pair will be created with the count as the key and the word as the first value, 
+            # this is also why we need the dict to be a defaultdict(list) - so it can append to the list
+            anagramsDict[key].append(word)
+
+        return anagramsDict.values()
+
+#Time Complexity: O(n * k) where n is the number of strings and k is the maximum length of a string
+#Space Complexity: O(n * k)
---
+++ Top K Frequent Elements/Top K Frequent Elements.py
@@ -0,0 +1,24 @@
+#Medium
+
+#My solution/Very suboptimal solution:
+
+#I used a dictionary to contain the frequency of each number, then I used a while loop to find the maximum frequency k times
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #Number:Frequency
+        dictionary = {}
+        for num in nums:
+            if num in dictionary:
+                dictionary[num] += 1
+            else:
+                dictionary[num] = 1
+        val = k
+        l1 = []
+        while k > 0:
+            key = max(dictionary, key=dictionary.get)
+            l1.append(key)
+            del dictionary[key]
+            k -= 1
+        return l1
+#Time Complexity: O(n * k) where n is the number of elements in nums and k is the number of unique elements
+#Space Complexity: O(n) where n is the number of unique elements in nums
